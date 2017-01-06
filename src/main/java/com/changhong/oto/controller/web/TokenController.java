12c0fe1ea34d74eef34f465029fd0b4db06e2866
向阳 <@10.9.50.43>
2017-01-06 13:39:33 +0800

    国际化

--- src/main/java/com/changhong/oto/controller/web/TokenController.java
+++ src/main/java/com/changhong/oto/controller/web/TokenController.java
@@ -1,9 +1,12 @@
 package com.changhong.oto.controller.web;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.support.RequestContext;
 
 import com.alibaba.fastjson.JSONObject;
 import com.changhong.oto.common.utils.AjaxResult;
@@ -30,7 +33,9 @@ public class TokenController {
 	
 	
 	@RequestMapping(value = "/index")
-	public String index() {
+	public String index(HttpServletRequest request) {
+		RequestContext requestContext = new RequestContext(request);
+		requestContext.getLocale();
 		return "index";
 	}
 }
